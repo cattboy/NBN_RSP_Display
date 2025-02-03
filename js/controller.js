fc5ae529d985f8369af2a92269b8407f313ed635
cattboy <cattboy@gmail.com>
2025-02-04 09:53:55 +1000

    search critera for later

--- js/controller.js
+++ js/controller.js
@@ -22,19 +22,20 @@ app.controller('ContactController', function($scope, $http) {
             $scope.$apply();
         });
          //Serve as a filter on the main list
-    $scope.criteriaMatch = function () {
-        return function (item) {
-            return (
-                (!document.criteria.service_nbn.checked || item.name.toLowerCase().indexOf('nbn') !== -1 || item.name.toLowerCase().indexOf('câble') !== -1) &&
-                (!document.criteria.service_vision.checked || item.name.toLowerCase().indexOf('vision') !== -1 || item.name.toLowerCase().indexOf('fibre') !== -1 || item.name.toLowerCase().indexOf('fibe') !== -1 || 
-                item.name.toLowerCase().indexOf('fttn') !== -1) &&
-                (!document.criteria.service_dsl.checked || item.name.toLowerCase().indexOf('dsl') !== -1) &&
-                (!document.criteria.service_other.checked || (item.name.toLowerCase().indexOf('fiber') === -1 && item.name.toLowerCase().indexOf('fibre') === -1 && item.name.toLowerCase().indexOf('fibe') === -1 
-                && item.name.toLowerCase().indexOf('fttn') === -1 && item.name.toLowerCase().indexOf('câble') === -1 && item.name.toLowerCase().indexOf('cable') === -1 && item.name.toLowerCase().indexOf('dsl') === -1)) &&
-                item.down_speed >= document.criteria.min_down_speed.value
-                    && (item.limit >= document.criteria.min_download_limit.value || item.limit === null)
-                    && (item.price <= document.criteria.montly_fee.value || document.criteria.montly_fee.value == '')
-                );
-        };
-    };
+    // $scope.criteriaMatch = function () {
+    //     return function (item) {
+    //         return (
+    //             (!document.criteria.service_nbn.checked || item.name.toLowerCase().indexOf('nbn') !== -1) &&
+    //             (!document.criteria.service_vision.checked || item.name.toLowerCase().indexOf('vision') !== -1 
+    //             || item.name.toLowerCase().indexOf('fibre') !== -1 || item.name.toLowerCase().indexOf('fibe') !== -1 || 
+    //             item.name.toLowerCase().indexOf('fttn') !== -1) &&
+    //             (!document.criteria.service_dsl.checked || item.name.toLowerCase().indexOf('dsl') !== -1) &&
+    //             (!document.criteria.service_other.checked || (item.name.toLowerCase().indexOf('fiber') === -1 && item.name.toLowerCase().indexOf('fibre') === -1 && item.name.toLowerCase().indexOf('fibe') === -1 
+    //             && item.name.toLowerCase().indexOf('fttn') === -1 && item.name.toLowerCase().indexOf('câble') === -1 && item.name.toLowerCase().indexOf('cable') === -1 && item.name.toLowerCase().indexOf('dsl') === -1)) &&
+    //             item.down_speed >= document.criteria.min_down_speed.value
+    //                 && (item.limit >= document.criteria.min_download_limit.value || item.limit === null)
+    //                 && (item.price <= document.criteria.montly_fee.value || document.criteria.montly_fee.value == '')
+    //             );
+    //     };
+    // };
 });

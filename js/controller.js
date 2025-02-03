b1bfd9f8bc3a1725ed652c273b93eddb15c12653
cattboy <cattboy@gmail.com>
2025-02-04 08:53:45 +1000

    Array of objects not of arrays

--- js/controller.js
+++ js/controller.js
@@ -7,18 +7,16 @@ app.controller('ContactController', function($scope, $http) {
     $http.get('./OUTPUT/nbn_providers_20250203_175414.json')
         .success(function(data) {
             console.log('Data loaded successfully:', data); // Debug log
-            data.forEach(function(item) {
-                $scope.contacts.push({
-                    name: item[0],
-                    phone: item[1],
-                    website: item[2]
-                });
-            });
+            // The data is already in the correct format, just assign it directly
+            $scope.contacts = data;
         })
         .error(function(error, status) {
             console.error('Error loading contacts - Status:', status);
             console.error('Error details:', error);
             
-
+            // Add some sample data in case of error
+            $scope.contacts = [
+                {name: "Sample Company", phone: "555-0101", website: "http://www.sample.com"}
+            ];
         });
 });

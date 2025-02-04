f70c9c9bb8f5093f68d3092dace4c62117ff2ba4
cattboy <cattboy@gmail.com>
2025-02-04 10:17:15 +1000

    1 radio button at a time selection

--- js/controller.js
+++ js/controller.js
@@ -60,19 +60,12 @@ app.controller('ContactController', function($scope, $http) {
          //Serve as a filter on the main list
     $scope.criteriaMatch = function () {
         return function (item) {
-            return (
-                (!document.criteria.service_nbn.checked || item.name.toLowerCase().indexOf('nbn') !== -1) &&
-                (!document.criteria.service_vision.checked || item.name.toLowerCase().indexOf('vision') !== -1 )
-
-                // || item.name.toLowerCase().indexOf('fibre') !== -1 || item.name.toLowerCase().indexOf('fibe') !== -1 || 
-                // item.name.toLowerCase().indexOf('fttn') !== -1) &&
-                // (!document.criteria.service_dsl.checked || item.name.toLowerCase().indexOf('dsl') !== -1) &&
-                // (!document.criteria.service_other.checked || (item.name.toLowerCase().indexOf('fiber') === -1 && item.name.toLowerCase().indexOf('fibre') === -1 && item.name.toLowerCase().indexOf('fibe') === -1 
-                // && item.name.toLowerCase().indexOf('fttn') === -1 && item.name.toLowerCase().indexOf('câble') === -1 && item.name.toLowerCase().indexOf('cable') === -1 && item.name.toLowerCase().indexOf('dsl') === -1)) &&
-                // item.down_speed >= document.criteria.min_down_speed.value
-                //     && (item.limit >= document.criteria.min_download_limit.value || item.limit === null)
-                //     && (item.price <= document.criteria.montly_fee.value || document.criteria.montly_fee.value == '')
-                );
+            if ($scope.service_type === 'nbn') {
+                return item.name.toLowerCase().indexOf('nbn') !== -1;
+            } else if ($scope.service_type === 'vision') {
+                return item.name.toLowerCase().indexOf('vision') !== -1;
+            }
+            return true; // fallback if no service type is selected
         };
     };
 } );
